8d54bf5fca6e667138778c431951837b3d13fec2
annibuliful <lagmanmc@gmail.com>
2022-10-12 18:40:52 +0700

    feat: init

---
+++ index.js
@@ -0,0 +1,55 @@
+/*
+map
+
+1 1 0 0 0 0
+1 1 0 0 0 0
+0 0 0 1 0 0
+0 1 0 0 1 1
+0 1 0 0 1 1
+
+fill(map, 1, 3)
+*/
+
+function spill(map, row, column) {
+  if (map?.[row]?.[column] === undefined) {
+    return map;
+  }
+
+  if (map?.[row]?.[column] === 1) {
+    map[row][column] = 0;
+    if (map?.[row]?.[column + 1] === 1) {
+      spill(map, row, column + 1);
+      map[row][column + 1] = 0;
+    }
+
+    if (map?.[row]?.[column - 1] === 1) {
+      spill(map, row, column - 1);
+      map[row][column - 1] = 0;
+    }
+
+    if (map?.[row + 1]?.[column] === 1) {
+      spill(map, row + 1, column);
+      map[row + 1][column] = 0;
+    }
+
+    if (map?.[row - 1]?.[column] === 1) {
+      spill(map, row - 1, column);
+      map[row - 1][column] = 0;
+    }
+  }
+
+  return map;
+}
+
+module.exports = spill;
+
+// const TILE_MAP = [
+//   [1, 1, 1, 0, 0, 0],
+//   [1, 1, 1, 0, 0, 0],
+//   [1, 1, 1, 1, 0, 0],
+//   [0, 1, 0, 0, 1, 1],
+//   [0, 1, 0, 0, 1, 1],
+// ];
+
+// console.log('TILE_MAP', TILE_MAP);
+// console.log('spill(TILE_MAP,0,0)', spill(TILE_MAP, 2, 3));
